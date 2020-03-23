5b8aa6ff00602069308cf64107c2d51936dcc9ac
Alberto Vecina Sánchez <albertovecina@gmail.com>
2020-03-23 10:44:49 +0100

    Initial Commit

---
+++ filmoteca/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  filmoteca
+//
+//  Created by Alberto Vecina Sánchez on 23/03/2020.
+//  Copyright © 2020 VSA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ filmotecaTests/filmotecaTests.swift
@@ -0,0 +1,34 @@
+//
+//  filmotecaTests.swift
+//  filmotecaTests
+//
+//  Created by Alberto Vecina Sánchez on 23/03/2020.
+//  Copyright © 2020 VSA. All rights reserved.
+//
+
+import XCTest
+@testable import filmoteca
+
+class filmotecaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
